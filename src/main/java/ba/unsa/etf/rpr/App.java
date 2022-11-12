3020bda4378f1371cd3907ea413b06982db7ce69
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2022-11-12 18:32:09 +0100

    new comment

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,12 +2,12 @@ package ba.unsa.etf.rpr;
 
 /**
  * Hello world!
- *
+ * comment by vmujic1
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Dobro dosli u TUT 3" );
     }
 }
